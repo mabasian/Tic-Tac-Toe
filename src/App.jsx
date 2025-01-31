2125c3b1f546b4b4095fd863cb637ae2b5e60dca
mabasian <54101509+mabasian@users.noreply.github.com>
2025-01-31 22:45:18 +0100

    rurning players

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,33 @@
+import { useState } from 'react'
+
 import Player from './components/Player'
 import GameBoard from './components/GameBoard'
 function App() {
+  const [activePlayer, setActivePlayer] = useState('X')
+
+  function handlePlayerChange() {
+    setActivePlayer((curActivePlayer) => (curActivePlayer === 'X' ? 'O' : 'X'))
+  }
+
   return (
     <main>
       <div id="game-container">
-        <ol id="players">
-          <Player initialName="Player 1" symbol="X" />
-          <Player initialName="Player 2" symbol="O" />
+        <ol id="players" className="highlight-player">
+          <Player
+            initialName="Player 1"
+            symbol="X"
+            isActive={activePlayer === 'X'}
+          />
+          <Player
+            initialName="Player 2"
+            symbol="O"
+            isActive={activePlayer === 'O'}
+          />
         </ol>
-        <GameBoard />
+        <GameBoard
+          onSelectSquare={handlePlayerChange}
+          activePlayerSymbol={activePlayer}
+        />
       </div>
       LOGO
     </main>
