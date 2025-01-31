f4ab2127b520802ed58bab4075b2311795294aa4
mabasian <54101509+mabasian@users.noreply.github.com>
2025-01-31 20:02:36 +0100

    Update pret

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,23 +1,25 @@
-import { useState } from "react"
+import { useState } from "react";
 
-export default function Player({name, symbol}) {
-const [isEditing, setIsEditing] = useState(false)
+export default function Player({ name, symbol }) {
+  const [isEditing, setIsEditing] = useState(false);
 
-function handleEditClick() {
-    setIsEditing(true)
-}
+  function handleEditClick() {
+    setIsEditing(true);
+  }
 
-let playerName = <span class="player-name">{name}</span>
+  let playerName = <span class="player-name">{name}</span>;
 
-if(isEditing) {
-    playerName = <input type="text" required />
-}
+  if (isEditing) {
+    playerName = <input type="text" required />;
+  }
 
-    return <li>
-    <span class = "player">
-    <span class="player-name">{playerName}</span>
-    <span class="player-symbol">{symbol}</span>
-    </span>
-    <button onClick={handleEditClick}>Edit</button>
+  return (
+    <li>
+      <span class="player">
+        <span class = "player-name">{playerName}</span>
+        <span class="player-symbol">{symbol}</span>
+      </span>
+      <button onClick={handleEditClick}>Edit</button>
     </li>
+  );
 }
