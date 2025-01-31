0fb394357037dcd92e20670e9cb2326e94b596bb
mabasian <54101509+mabasian@users.noreply.github.com>
2025-01-31 19:42:51 +0100

    add update

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react"
+
 export default function Player({name, symbol}) {
+const [isEditing, setIsEditing] = useState(false)
+
+function handleEditClick() {
+    setIsEditing(true)
+}
+
+let playerName = <span class="player-name">{name}</span>
+
+if(isEditing) {
+    playerName = <input type="text" required />
+}
+
     return <li>
     <span class="player">
-    <span class="player-name">{name}</span>
+    <span class="player-name">{playerName}</span>
     <span class="player-symbol">{symbol}</span>
     </span>
-    <button>Edit</button>
+    <button onClick={handleEditClick}>Edit</button>
     </li>
 }
