cbb425a844b2390670aff60e4d8777f75e737991
mabasian <54101509+mabasian@users.noreply.github.com>
2025-02-21 22:27:03 +0100

    winner

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,13 @@ import { useState } from 'react'
 import Player from './components/Player'
 import GameBoard from './components/GameBoard'
 import Log from './components/Log'
+import { WINNING_COMBINATIONS } from './winning-combinations'
+
+const initialGameBoard = [
+  [null, null, null],
+  [null, null, null],
+  [null, null, null],
+]
 
 function deriveActivePlayer(gameTurns) {
   if (gameTurns.length > 0 && gameTurns[0].player === 'X') {
@@ -14,6 +21,34 @@ function App() {
   const [gameTurns, setGameTurns] = useState([])
 
   const activePlayer = deriveActivePlayer(gameTurns)
+
+  let gameBoard = initialGameBoard
+
+  for (const turn of gameTurns) {
+    const { square, player } = turn
+    const { row, col } = square
+
+    gameBoard[row][col] = player
+  }
+
+  let winner = null
+
+  for (const combination of WINNING_COMBINATIONS) {
+    const firstSquareSymbol =
+      gameBoard[combination[0].row][combination[0].column]
+    const secondSquareSymbol =
+      gameBoard[combination[1].row][combination[1].column]
+    const thirdSquareSymbol =
+      gameBoard[combination[2].row][combination[2].column]
+
+    if (
+      firstSquareSymbol &&
+      firstSquareSymbol === secondSquareSymbol &&
+      firstSquareSymbol === thirdSquareSymbol
+    ) {
+      winner = firstSquareSymbol
+    }
+  }
 
   function handlePlayerChange(rowIndex, colIndex) {
     setGameTurns((prevTurns) => {
@@ -41,7 +76,8 @@ function App() {
             isActive={activePlayer === 'O'}
           />
         </ol>
-        <GameBoard onSelectSquare={handlePlayerChange} turns={gameTurns} />
+        {winner && <p>{winner} wins!</p>}
+        <GameBoard onSelectSquare={handlePlayerChange} board={gameBoard} />
       </div>
       <Log turns={gameTurns} />
     </main>
